27396fc56e8d6cae6711ed213df356affd9647ea
Amirton Chagas <chagas@amirton.com>
2022-04-05 19:49:19 -0300

    Add initial SpringBoot Web project files

---
+++ src/main/java/com/project/professorallocation/ProfessorAllocationAmirtonApplication.java
@@ -0,0 +1,13 @@
+package com.project.professorallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationAmirtonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationAmirtonApplication.class, args);
+	}
+
+}

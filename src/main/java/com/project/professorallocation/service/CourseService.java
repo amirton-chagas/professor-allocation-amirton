cf68a7fcce16a9049702e326e0640561c357ab94
Amirton Chagas <chagas@amirton.com>
2022-04-18 19:48:12 -0300

    Add Service Layer

---
+++ src/main/java/com/project/professorallocation/service/CourseService.java
@@ -0,0 +1,15 @@
+package com.project.professorallocation.service;
+
+import org.springframework.stereotype.Service;
+
+import com.project.professorallocation.repository.CourseRepository;
+
+@Service
+public class CourseService {
+	private final CourseRepository repository;
+
+	public CourseService(CourseRepository repository) {
+		super();
+		this.repository = repository;
+	}
+}

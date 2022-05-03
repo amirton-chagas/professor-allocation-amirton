ca4595f06a8cb03efd02a49b0b5f6e1e7826f9b4
Amirton Chagas <chagas@amirton.com>
2022-05-03 07:19:01 -0300

    Implement find by professor and course in the Allocation Controller

--- src/main/java/com/project/professorallocation/controller/AllocationController.java
+++ src/main/java/com/project/professorallocation/controller/AllocationController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +35,70 @@ public class AllocationController {
 		List<Allocation> allItems = service.findAll();
 		
 		return new ResponseEntity<List<Allocation>>(allItems, HttpStatus.OK);
+	}
+	
+	@GetMapping(path = "/{item_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Allocation> findById(@PathVariable(name = "item_id") Long id) {
+		Allocation item = service.findById(id);
+
+		if (item == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(item, HttpStatus.OK);
+		}
+	}
+	
+	@GetMapping(path = "/professor/{item_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<List<Allocation>> findByProfessorId(@PathVariable(name = "item_id") Long id) {
+		List<Allocation> allItems = service.findByProfessorId(id);
+		
+		return new ResponseEntity<List<Allocation>>(allItems, HttpStatus.OK);
+	}
+	
+	@GetMapping(path = "/course/{item_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<List<Allocation>> findByCourseId(@PathVariable(name = "item_id") Long id) {
+		List<Allocation> allItems = service.findByCourseId(id);
+		
+		return new ResponseEntity<List<Allocation>>(allItems, HttpStatus.OK);
+	}
+	
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Allocation> create(@RequestBody Allocation item) {
+		try {
+			Allocation newItem = service.create(item);
+	
+			return new ResponseEntity<>(newItem, HttpStatus.CREATED);
+		} catch (Exception ex) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@PutMapping(path = "/{item_id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Allocation> update(@PathVariable(name = "item_id") Long id, @RequestBody Allocation item) {
+		try {
+			item.setId(id);
+			Allocation newItem = service.create(item);
+	
+			if (newItem == null) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			} else {
+				return new ResponseEntity<>(newItem, HttpStatus.CREATED);
+			}
+		} catch (Exception ex) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@DeleteMapping(path = "/{item_id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Void> delete(@PathVariable(name = "item_id") Long id) {
+		service.deleteById(id);
+
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
